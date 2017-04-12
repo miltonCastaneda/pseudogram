412d0ef71ee6a3cffbec9241d17f5851e26631ed
Milton Castañeda <milton1792@gmail.com>
2017-04-12 18:20:32 -0500

    Inicion de sesión con google, funcion de autenticación y control de error con dependencia firebase

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+// import logo from './logo.svg'
+import firebase from 'firebase'
+import './App.css'
 
 class App extends Component {
+
+  handleAuth () {
+    const provider = new firebase.auth.GoogleAuthProvider()
+
+    firebase.auth().signInWithPopup(provider)
+    .then(result => console.log(`${result.user.email} ha iciado sesión`))
+    .catch(error => console.log(`Error ${error.code}: ${error.message}`))
+  }
+
+  renderLoginButton (){
+    
+
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          
           <h2>pseudogram</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <button onClick={this.handleAuth}>
+            Login con Google  
+          </button>
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
